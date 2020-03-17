ba49a23c598aeb80eeb28a90d8450f2a2f58b3d7
chnnn <chnnn.git@gmail.com>
2020-03-17 22:47:20 +0900

    VarArgs

---
+++ algorithm_visualisation_Java/misc/Cp5_VarArgs.java
@@ -0,0 +1,20 @@
+public class Cp5_VarArgs{
+    //A Static Method
+    static Object obj = new Integer(1);
+    static Object i = new Integer(1);
+
+    static void printArray(Object[] args){
+	for(Object obj : args)
+	    System.out.print(" " + obj);
+	System.out.println();
+    }
+    public static void main (String[] args){
+	int i = 1;
+	Object c = (Object) i;
+	System.out.printlnc.getClass());
+
+	printArray(new Object[]{
+		new Integer(47), new Float(3.141)
+	    });
+    }
+}
