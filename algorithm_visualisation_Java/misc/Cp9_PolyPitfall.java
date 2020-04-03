daf3d89b99da805a44b37b64693578fe3eadce8c
chnnn <chnnn.git@gmail.com>
2020-04-03 19:14:47 +0900

    java elisp

--- algorithm_visualisation_Java/misc/Cp9_PolyPitfall.java
+++ algorithm_visualisation_Java/misc/Cp9_PolyPitfall.java
@@ -1,15 +1,24 @@
 class Super {
     public int i = 0;
     public int getI(){return i;}
+    static int j = 0;
+    public int getJ(){return j;}
 }
 class Sub extends Super{
     public int i = 1;
     public int getI(){return i;}
     public int getSuperI(){return super.i;}
+    static int j = 1;
+    public int getJ(){return j;}
+    public int getSuperJ(){return super.j;}
 }
 public class Cp9_PolyPitfall{
     public static void main (String[] args){
 	Super sup = new Sub();
-	System.out.println("sup.i " + sup.i + " sup.getI()" + sup.getI());
+	Sub sub01 = new Sub();
+	System.out.println("sup.i " + sup.i + " sup.getI()" + sup.getI()); //0 1
+	System.out.println("sup.j " + sup.j + " sup.getJ()" + sup.getJ()); //0 1
+	System.out.println("sub01.getSuperI()" + sub01.getSuperI()); //0
+	System.out.println("sub01.getSuperJ()" + sub01.getSuperJ()); //0
     }
 }
--- algorithm_visualisation_Java/misc/HelloWorld.java
+++ algorithm_visualisation_Java/misc/HelloWorld.java
@@ -1,7 +1,57 @@
-public class HelloWorld {
+import java.rmi.server.ObjID;
+
+import javax.naming.event.ObjectChangeListener;
+
+import sun.awt.AWTAccessor.SystemColorAccessor;
+import sun.jvm.hotspot.oops.ArgInfoData;
+
+public class HelloWorld{
+    @Test
+    public void worldx() throws Exception{
+	World x = new World();
+    }
+    
     public static void main(String[] args) {
+	B2 x = new ba(); 
+	hi x = new hi();
+	x = toString(x);::
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+        System.out.println(x);
+	System.out.println("a");
+	System.out.println();
+	HelloWorld y = new HelloWorld();
+	HelloWorld xx = new HelloWorld();
+	HelloWorld z = new HelloWorld();
+	HelloWorld r = new HelloWorld();
+	String x = new String("x");
+	System.out.println(ObjIntConsumer);
+	System.out.println();
+	HelloWorld;
+	System.out.println();
+	y.fucttttttttttion();
+	System.out.append();
+	System.class();
+	y.fucttttttttttion;
+	SystemColorAccessor;
+	you.fucttttttttttion();
+	y.fucttttttttttion();
+       
+	
     }
+    ()
+    /**
+	@Param String a, int b, String c, String d
+	@author k
+	docs body
+    */
+	
+    public static int fucttttttttttion(int a, int b, int...c){
+	return(0);
+    }
+	       
 
+	@Override
+	public String toString() {
+		return "HelloWorld []";	
 }
