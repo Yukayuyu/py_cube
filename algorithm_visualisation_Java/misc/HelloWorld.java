6c1a2910d413f80da684eb6a6dc36aacda3a9277
chnnn <chnnn.git@gmail.com>
2020-04-04 07:38:21 +0900

    hw

--- algorithm_visualisation_Java/misc/HelloWorld.java
+++ algorithm_visualisation_Java/misc/HelloWorld.java
@@ -1,57 +1,15 @@
-import java.rmi.server.ObjID;
-
-import javax.naming.event.ObjectChangeListener;
-
-import sun.awt.AWTAccessor.SystemColorAccessor;
-import sun.jvm.hotspot.oops.ArgInfoData;
-
 public class HelloWorld{
-    @Test
-    public void worldx() throws Exception{
-	World x = new World();
-    }
     
-    public static void main(String[] args) {
-	B2 x = new ba(); 
-	hi x = new hi();
-	x = toString(x);::
+    public static void main(final String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
-        System.out.println(x);
-	System.out.println("a");
-	System.out.println();
-	HelloWorld y = new HelloWorld();
-	HelloWorld xx = new HelloWorld();
-	HelloWorld z = new HelloWorld();
-	HelloWorld r = new HelloWorld();
-	String x = new String("x");
-	System.out.println(ObjIntConsumer);
-	System.out.println();
-	HelloWorld;
-	System.out.println();
-	y.fucttttttttttion();
-	System.out.append();
-	System.class();
-	y.fucttttttttttion;
-	SystemColorAccessor;
-	you.fucttttttttttion();
-	y.fucttttttttttion();
-       
-	
     }
-    ()
     /**
 	@Param String a, int b, String c, String d
 	@author k
 	docs body
     */
-	
-    public static int fucttttttttttion(int a, int b, int...c){
+    public static int fucttttttttttion(final int a, final int b, final int...c){
 	return(0);
     }
-	       
-
-	@Override
-	public String toString() {
-		return "HelloWorld []";	
 }
