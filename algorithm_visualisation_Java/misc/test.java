68a0aa27bc808b5ad0356462646402cc04a1831e
chnnn <chnnn.git@gmail.com>
2020-05-01 01:58:45 +0900

    hexdump

--- algorithm_visualisation_Java/misc/test.java
+++ algorithm_visualisation_Java/misc/test.java
@@ -1,7 +1,22 @@
 public class test {
-    static int i = 2;
-    public static void main(String[] args) {
-        test x = new test();
-        System.out.println("text + String: " + i);
+    public static void main(String[] args) throws Exception {
+        byte[] b = new byte[2];
+        b[0] = 100;
+        b[1] = 0x2E;
+        // String s = new String(x.b, "UTF-8");
+        // String s = (b, 0,1)
+        System.out.println(b.getClass());
+        // String s = new String("hi");
+        String s = new String(b, 0, 1, "UTF-8");
+        System.out.println(s);
+        String s2 = new String("ABCDEFG");
+        String base = new String("base");
+        StringBuilder sb = new StringBuilder();
+        sb.append(base);
+        sb.append(s2, 1, 4);
+        System.out.println(sb.toString());
+        byte k = 0x2E;
+        System.out.println(k < 47);
+
     }
 }
