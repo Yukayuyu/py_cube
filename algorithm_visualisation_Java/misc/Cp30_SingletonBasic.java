2b6fdaa0f6cfbc54eb6661f17b1633d9234a1be1
chnnn <chnnn.git@gmail.com>
2020-05-26 13:51:40 +0900

    serializable, stream

---
+++ algorithm_visualisation_Java/misc/Cp30_SingletonBasic.java
@@ -0,0 +1,16 @@
+class Cp30_SingletonBasicSingleton {
+    private static final Cp30_SingletonBasicSingleton INSTANCE = new Cp30_SingletonBasicSingleton();
+
+    private Cp30_SingletonBasicSingleton() {}
+
+    public static Cp30_SingletonBasicSingleton getInstance() {
+        return INSTANCE;
+    }
+}
+
+
+public class Cp30_SingletonBasic {
+    public static void main(String[] args) {
+        Cp30_SingletonBasicSingleton s = Cp30_SingletonBasicSingleton.getInstance();
+    }
+}
