b06b27a9f55dc8035bab965713164e95bf7bb2f5
chnnn <chnnn.git@gmail.com>
2020-06-16 12:03:50 +0900

    sql misc

--- algorithm_visualisation_Java/misc/sqlPractice/01/01mySolution.sql
+++ algorithm_visualisation_Java/misc/sqlPractice/01/01mySolution.sql
@@ -55,7 +55,10 @@ SELECT * FROM score WHERE degree > (SELECT degree FROM score WHERE sno = '109' A
 -- 22、生徒番号108の生徒と同じ年に生まれた全部生徒のSno、Sname、Sbirthday列。
 SELECT student.sno, sname, sbirthday FROM student WHERE YEAR(sbirthday) = (SELECT YEAR(sbirthday) FROM student WHERE sno = '108');
 -- 23、“小林“先生の生徒たちの成績。
-SELECT degree FROM score LEFT JOIN course ON score.cno = course.cno LEFT JOIN teacher ON course.tno = teacher.tno WHERE tname = 'Kobayashi';
+SELECT degree FROM score LEFT JOIN course ON score.cno = course.cno
+JOIN teacher ON course.tno = teacher.tno AND tname = 'Kobayashi';
+
+SELECT s.sno, s.sname, s.ssex FROM student s JOIN score sc ON s.sno = sc.sno WHERE sc.cno IN (SELECT c.cno FROM course c JOIN teacher t ON c.tno = t.tno WHERE tname = 'Kobayashi');
 -- 24、選修生徒人数は5名上の課程の担当教師の姓名。
 SELECT tname FROM teacher LEFT JOIN course ON course.tno = teacher.tno LEFT JOIN score ON score.cno = course.cno  GROUP BY course.cno HAVING COUNT(sno) > 5;
 -- 25、95033と 95031のクラスの全部学生の情報。
@@ -68,9 +71,14 @@ SELECT sno, degree FROM score LEFT JOIN course ON course.cno = score.cno LEFT JO
 SELECT tname, PROF FROM teacher WHERE depart = 'CS' AND PROF NOT IN (SELECT PROF FROM teacher WHERE depart = 'Electric');
 -- 29、 課程“3-105“を履修している成績が課程“3-245”の成績のいずれより良い生徒のCno、Sno、Degree, Degree降順で取得。
 SELECT cno, student.sno, degree FROM student LEFT JOIN score ON student.sno = score.sno WHERE score.cno = '3-105' AND degree > ANY (SELECT degree FROM score WHERE cno = '3-245');
--- ? 30、課程“3-105“を選修している成績が課程“3-245”の成績より良い生徒のCno、Sno、Degree, Degree降順で取得。
+
+-- 30、課程“3-105“を選修している成績が課程“3-245”の成績より良い生徒のCno、Sno、Degree, Degree降順で取得。
+-- IMPORTANT tmpA cannot be called like: tmpA AS tmpB;
 -- ! wrong query SELECT * FROM score WHERE degree>ALL(SELECT degree FROM score WHERE cno='3-245') ORDER BY degree DESC;
-SELECT sno, degree FROM score A WHERE cno = '3-245' AND degree IS NOT NULL and degree < (SELECT degree FROM score B WHERE cno = '3-105' AND A.sno = B.sno);
+-- step 1:
+SELECT * FROM (SELECT sno, degree, cno FROM score A WHERE cno = '3-105') AS tmpA JOIN (SELECT sno, degree, cno FROM score A WHERE cno = '3-245')AS tmpB ON tmpA.sno = tmpB.sno;
+-- step 2:
+SELECT tmpA.sno, tmpA.degree FROM (SELECT sno, degree, cno FROM score A WHERE cno = '3-105') AS tmpA JOIN (SELECT sno, degree, cno FROM score A WHERE cno = '3-245')AS tmpB ON tmpA.sno = tmpB.sno AND tmpA.degree > tmpB.degree;
 -- 31、先生と生徒全員のname、sexとbirthday
 SELECT sname AS `name`, ssex AS sex, sbirthday AS birthday FROM student
 UNION
@@ -80,7 +88,13 @@ SELECT sname AS `name`, ssex AS sex, sbirthday AS birthday FROM student
 UNION
 SELECT tname AS `name`, TSEX AS sex, tbirthday AS birthday FROM teacher WHERE TSEX = 'F';
 -- 33、成績が該当課程の平均成績より低いの生徒の成績表
-SELECT degree, cno, sno FROM score A WHERE degree < (SELECT AVG(degree) FROM score B WHERE A.cno = B.cno GROUP BY cno );
+SELECT degree, cno, sno FROM score A WHERE degree < (SELECT AVG(degree) FROM score B WHERE A.cno = B.cno GROUP BY cno);
+SELECT tblA.*, tblB.savg FROM (SELECT degree, cno, sno FROM score A WHERE degree < (SELECT AVG(degree) FROM score B GROUP BY cno HAVING A.cno = B.cno)) AS tblA LEFT JOIN (SELECT cno, AVG(degree) AS savg FROM score GROUP BY cno) AS tblB ON tblA.cno = tblB.cno;
+-- best:
+SELECT degree, score.cno, sno, avgT1.savg FROM score LEFT JOIN 
+(SELECT cno, AVG(degree) AS savg FROM score GROUP BY cno) AS avgT1
+ON score.cno = avgT1.cno
+WHERE score.degree < avgT1.savg;
 -- 34、課程を開講している先生のTnameとDepart.
 SELECT tname, depart FROM teacher LEFT JOIN course ON teacher.tno = course.tno WHERE cno IS NOT NULL;
 -- 36、男性人数は2名以上超えているクラスのCNO
@@ -108,11 +122,15 @@ SELECT sname FROM student WHERE ssex = (SELECT ssex FROM student WHERE sname = '
 SELECT score.sno, score.cno, degree FROM score LEFT JOIN student ON score.sno = student.sno
 LEFT JOIN course ON score.cno = course.cno
 WHERE cname = 'Computer' AND ssex = 'M';
--- ? 46、”小林”先生の担当課程に成績の一番良い生徒
-SELECT score.sno FROM score LEFT JOIN course ON score.cno = course.cno
+-- IMPORTANT!!! 46、”小林”先生の担当課程に成績の一番良い生徒
+SELECT ktbl.kst AS student, MAX(ktbl.kdg) AS HighestScore FROM (SELECT score.sno AS kst, score.degree AS kdg FROM score LEFT JOIN course ON score.cno = course.cno
 LEFT JOIN teacher ON teacher.tno = course.tno
-WHERE tname = 'Kobayashi' AND score.degree = SELECT (MAX() FROM score);
+WHERE tname = 'Kobayashi') AS ktbl;
 -- 47、各課程の成績最高の生徒
--- 48、各課程の成績最高の生徒の前2位
+SELECT score.sno, MAX(score.degree) AS 'MaxDegree' FROM score GROUP BY cno;
+-- ? TOP 10? 48、各課程の成績最高の生徒の前2位
+SELECT DISTINCT FROM score.sno, score.degree FROM score ORDER BY cno, degree LIMIT 2;
 -- 49、各生徒の番号、姓名、選修課程数、全部課程の総点数
+SELECT student.sno, student.sname, COUNT(cno), SUM(degree) FROM student JOIN score ON student.sno = score.sno GROUP BY student.sno;
 -- 50、今月また来月、誕生日の生徒
+SELECT student.sno, student.sname FROM student WHERE ((MONTH(sbirthday) - MONTH(NOW())) <= 2);
